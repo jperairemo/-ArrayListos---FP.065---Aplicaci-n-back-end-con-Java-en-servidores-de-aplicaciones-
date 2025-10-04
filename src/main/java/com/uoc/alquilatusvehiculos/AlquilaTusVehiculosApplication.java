1fecef500d5aea94b5acb7cd4fc78532196926f1
ramoralesloz <ramoralesloz@gmail.com>
2025-10-04 20:14:25 +0200

    P1: Proyecto base AlquilaTusVehiculos (Spring Boot + endpoint /api/hello)

---
+++ src/main/java/com/uoc/alquilatusvehiculos/AlquilaTusVehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.uoc.alquilatusvehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilaTusVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilaTusVehiculosApplication.class, args);
+	}
+
+}
